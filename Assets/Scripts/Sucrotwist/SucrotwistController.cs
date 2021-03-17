93ada4b3c3c8327ff8696a761c2cfbc9f83f148f
Andrei <borodin_a_o@sc.vsu.ru>
2021-03-17 11:21:23 +0300

    menu added + changes

---
+++ Assets/Scripts/Sucrotwist/SucrotwistController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class SucrotwistController : MonoBehaviour
+{
+    [SerializeField] private string sucrotwistTag = "Player";
+    
+    private GameObject _sucrotwistObject;
+    private Animator _sucrotwistAnimator;
+
+    private GameObject Sucrotwist
+    {
+        get
+        {
+            if (_sucrotwistObject == null)
+            {
+                _sucrotwistObject = GameObject.FindGameObjectWithTag(sucrotwistTag);
+            }
+
+            return _sucrotwistObject;
+        }
+    }
+
+    private Animator SucrotwistAnimator
+    {
+        get
+        {
+            if (_sucrotwistAnimator == null)
+            {
+                _sucrotwistAnimator = Sucrotwist.GetComponent<Animator>();
+            }
+
+            return _sucrotwistAnimator;
+        }
+    }
+
+    public void PlayAnimation()
+    {
+        SucrotwistAnimator.Play("ComplexAnimation");
+    }
+}
